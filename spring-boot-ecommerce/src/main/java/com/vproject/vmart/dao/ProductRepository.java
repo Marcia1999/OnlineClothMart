9aaf840faae370c208716325ceba5cc95345fb69
Marcia1999 <63154325+Marcia1999@users.noreply.github.com>
2020-07-07 21:47:19 +0530

    Add files via upload

---
+++ spring-boot-ecommerce/src/main/java/com/vproject/vmart/dao/ProductRepository.java
@@ -0,0 +1,16 @@
+package com.vproject.vmart.dao;
+
+import com.vproject.vmart.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@CrossOrigin("http://localhost:4200")
+public interface ProductRepository extends JpaRepository<Product,Long>{
+
+    // Below is a query method to find by id
+    Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
+    Page<Product> findByNameContaining(@RequestParam("name") String name, Pageable pageable);
+}
